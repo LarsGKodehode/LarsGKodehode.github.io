dbc755a4ce1ae26099fe16e676a547b8f9de4146
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-05-13 13:51:15 +0200

    updatet from src

--- page-hierarchy/lessons/Scrimba-3.4/pwGene.js
+++ page-hierarchy/lessons/Scrimba-3.4/pwGene.js
@@ -1,23 +1,39 @@
 // container for available output fields
-let outputFields = []
-const docOutputFields = document.getElementsByClassName("output-field");
+let outputFields = [];
+const documentOutputFields = document.getElementsByClassName("output-field");
 
-// pre calculated value for number of symbols in password
-const numberOfSymbols = 16;
+// how many symbols in password
+const passwordLength = 16;
 
-// this is a sensible utf-8 range
+// this is a sensible selection of utf-8 charecters
 const alphabetStart = 33;
 const alphabetEnd = 127;
 const alphabetSize = alphabetEnd - alphabetStart;
 
-
-
-function submit() {
-  for (entry in docOutputFields) {
-    let pwGene = "";
-    for (i = 0; i < numberOfSymbols; i++) {
-      pwGene += String.fromCharCode(Math.floor(Math.random() * alphabetSize + alphabetStart));
+function generatePassword() {
+  for (field in documentOutputFields) {
+    let newPassword = "";
+    for (i = 0; i < passwordLength; i++) {
+      newPassword += String.fromCharCode(Math.floor(Math.random() * alphabetSize + alphabetStart));
     };
-    docOutputFields[entry].value = pwGene;
+    documentOutputFields[field].value = newPassword;
   };
 };
+
+function switchTheme() {
+  console.log("button clicked");
+  let currentTheme = document.body.dataset.theme;
+  switch (currentTheme) {
+    case "dark":
+      document.body.dataset.theme = "light";
+      console.log("new theme is light")
+      break;
+    case "light":
+      document.body.dataset.theme = "dark";
+      console.log("new theme is dark")
+      break;
+  };
+};
+
+const themeSwitch = document.getElementById("theme-switcher");
+themeSwitch.addEventListener("click", switchTheme);

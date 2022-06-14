364f28730c1140d3ad0f5c57ee6bb8c7cad43464
LarsGKodehode <lars.gunnar.kodehodet@gmail.com>
2022-06-14 21:59:19 +0200

    update from src

--- page-hierarchy/lessons/dokker-oppgaver/todo-list/todo-list.js
+++ page-hierarchy/lessons/dokker-oppgaver/todo-list/todo-list.js
@@ -1,6 +1,6 @@
 /* CONTENTS:
   1. Global Variables
-  2. Function to append task
+  2. Function to append task element
   3. Helper Functions
   4. Functionality Functions
   5. Keyboard Shortcuts
@@ -11,33 +11,35 @@ const inputField = document.getElementById("input-field");
 const buttonSubmit = document.getElementById("input-button-submit");
 const buttonSort = document.getElementById("button-sort");
 const listTarget = document.getElementById("task-list-insertion-point");
-let taskList = [];
+let taskList = []; // array to hold tasks for sorting and rerendering
 
 
 
-// ----- 2. Function create list element k -----
+// ----- 2. Function to append task element -----
+/*
+this function is really messy
+*/
 function addListElement(description, target=listTarget) {
   // --- Element Wrapper
   const nodeElementWrapper = {
-    type: "li",
+    nodeType: "li",
     className: "list-element-wrapper",
   };
   const elementWrapper = createNode(nodeElementWrapper);
-  // add ordering through hasinh the discription
-  elementWrapper.dataset.hash = simpleHash(description);
 
 
   // --- Overlay
   const nodeOverlay = {
-    type: "div",
-    className: "overlay-task-done hidden",
+    nodeType: "div",
+    className: "overlay-task-done hidden not-clickable",
+
   };
   elementWrapper.appendChild(createNode(nodeOverlay));
 
 
   // --- Section Texts
   const nodeTextWrapper = {
-    type: "div",
+    nodeType: "div",
     className: "texts-wrapper",
   };
   const textWrapper = createNode(nodeTextWrapper);
@@ -45,82 +47,97 @@ function addListElement(description, target=listTarget) {
 
   // text description
   const nodeDescriptionElement = {
-    type: "p",
+    nodeType: "p",
     textContent: description,
   };
   textWrapper.appendChild(createNode(nodeDescriptionElement));
 
   // text details
-  const nodeDetailElement = {
-    type: "small",
-    className: "button-add-details",
-    textContent: "Add details",
-  };
-  textWrapper.appendChild(createNode(nodeDetailElement));
+  /*
+  TODO: not yet implemented
+  */
+  // const nodeDetailElement = {
+  //   nodeType: "small",
+  //   className: "button-add-details",
+  //   textContent: "Add details",
+  // };
+  // textWrapper.appendChild(createNode(nodeDetailElement));
 
   
   //  --- Section Buttons
   const nodeButtonWrapper = {
-    type: "div",
+    nodeType: "div",
     className: "buttons-wrapper",
   };
   const buttonWrapper = createNode(nodeButtonWrapper);
   elementWrapper.appendChild(buttonWrapper);
 
   // button done
-  const buttonDone = document.createElement("input");
-  buttonDone.type = "button";
-  buttonDone.className = "button button-done";
-  buttonDone.value = "DONE";
+  const nodeButtonDone = {
+    nodeType: "input",
+    type: "button",
+    className: "button button-done",
+    value: "DONE",
+  };
+  const buttonDone = createNode(nodeButtonDone);
   buttonWrapper.appendChild(buttonDone);
 
   // button delete
-  const buttonDelete = document.createElement("input");
-  buttonDelete.type = "button";
-  buttonDelete.className = "button button-delete";
-  buttonDelete.value = "DELETE";
+  const nodeButtonDelete = {
+    nodeType: "input",
+    type: "button",
+    className: "button button-delete",
+    value: "DELETE",
+  }
+  const buttonDelete = createNode(nodeButtonDelete);
   buttonWrapper.appendChild(buttonDelete);
 
   // add interactions
   buttonDone.addEventListener("click", (e) => taskComplete(e));
   buttonDelete.addEventListener("click", (e) => taskDelete(e));
 
-  // add to task list
-  taskList.push(elementWrapper);
+  // add to DOM
+  target.appendChild(elementWrapper);
 };
 
 
 
 // ----- 3. Helper Functions -----
+
+// creates a new element from object
 function createNode(createInfo) {
-  const newNode = document.createElement(createInfo.type);
+  const newNode = document.createElement(createInfo.nodeType);
   for ([key, value] of Object.entries(createInfo)) {
-    if (key === "type") {continue};
+    if (key === "nodeType") {continue};
     newNode[key] = value;
   };
   return newNode;
 };
 
-// hashing algorithm
-// TODO: this does not work as intended currently, read some more
-// problem is first characters gets assigned to low values, might just get the value of the 32 first ones
-function simpleHash(string) {
-  let hash = 0;
-  if (string.length === 0) {return hash};
-  for (let i = 0; i < string.length; i++) {
-    hash += string.charCodeAt(i) * (string.length - i); // problematic portion
+// rerenders stored tasks
+function renderTasks() {
+  // clears task list
+  while(listTarget.firstChild) {
+    listTarget.removeChild(listTarget.firstChild);
   };
-  hash |= 0; // casts value to a 32bit int
-  return hash;
+  // readds tasks to DOM
+  for (task of taskList) {
+    addListElement(task);
+  };
 };
 
 
-
 // ----- 4. Funtionality Functions -----
+
 // function sections
+
 function taskAdd() {
   if (inputField.value == /^\s*$/) {return}; // regex to check for empty string
+  // add to task list
+  taskList.push(inputField.value);
+  // add to DOM
   addListElement(inputField.value);
+  // clears input field
   inputField.value = "";
 };
 
@@ -128,48 +145,45 @@ function taskComplete(e) {
   e.target.parentElement.previousSibling.previousSibling.classList.toggle("hidden");
 };
 
+/*
+TODO: actually 'remove element from taskList
+*/
 function taskDelete(e) {
-  // hardcoded value here
   e.target.parentElement.parentElement.remove();
 };
 
 function focusInput() {
   inputField.focus();
-}
-
-// rerenders stored tasks
-function renderTasks() {
-  for (task of taskList) {
-    targetList.appendChild(task);
-  };
 };
 
-// sort tasks
-function sortTasks() {
-  for (node of taskList) {
-    console.log(node);
-    node.style.order = node.dataset.hash;
-    console.log(node);
-  };
-};
 
 // event listener section
+
 // submit form
 buttonSubmit.addEventListener("click", (e) => {
-  if (inputField.value.match(/^\s*$/)) {return}
+  if (inputField.value.match(/^\s*$/)) {return} // ignores empty input
   taskAdd();
 });
 
-buttonSort.addEventListener("click", (e) => sortTasks());
+// reorder list
+buttonSort.addEventListener("click", (e) => {
+  taskList.sort();
+  renderTasks();
+});
 
 
 
 // ----- 5. Keyboard Shortcuts -----
+
 // keybinding listner
-document.addEventListener("keydown", (e) => {
-  if (!(e.code in keybindings)) {return}; // check if we have registred a keybinding
-  if (!document.activeElement.type === "text") {e.preventDefault()}; // prevents hijacking of input if active text input
-  keybindings[e.code](); // run hotkeyed function
+/*
+currently only handles single key inputs, no "ctrl(cmd) + key" combos
+ */
+document.addEventListener("keydown", (event) => {
+  if (!(event.code in keybindings)) {return}; // check if we have registred a keybinding
+  if (document.activeElement.type === "text") {return}; // check if text input is in focus
+  event.preventDefault(); // bypasses default event
+  keybindings[event.code](event); // run hotkeyed function
 });
 
 // keybindings storage
